c50c514a6296634ae6ce9a8346891617210a6b43
dinar <bqcrystal75@gmail.com>
2024-10-05 12:43:54 +0500

    Добавлен код в файл homework2

---
+++ homework2/pr7_2.py
@@ -0,0 +1,10 @@
+user_input = int(input("Введите число:"))
+binary = bin(user_input)
+octt = oct(user_input)
+
+
+print("Первые два элемента обозначают систему счисления(0b - двоичная, 0o - воьсмиричная)", binary, octt)
+
+input()
+
+time.sleep()
